cafa2622c9f0f0ba60c3324f998efd2489b37c0d
Michael Claybaugh <michael.claybaugh@gmail.com>
2019-09-11 10:39:06 -0400

    main, ul and dl format

--- src/main.js
+++ src/main.js
@@ -1,9 +1,48 @@
 const yaml = require('js-yaml');
 const fs   = require('fs');
 
+let data, format;
 try {
-  const doc = yaml.safeLoad(fs.readFileSync('data.yml'));
-  console.log(doc);
+  data = yaml.safeLoad(fs.readFileSync('data.yml'));
+  format = yaml.safeLoad(fs.readFileSync('format.yml'));
 } catch (e) {
   console.log(e);
+}
+
+let entries = Object.entries(data);
+for (let [heading, list] of entries) {
+  console.log(fheading(format, heading));
+  console.log(format.beforeList);
+  for (let item of list) {
+    let entries = flat(Object.entries(item)),
+      key = entries[0],
+      value = entries[1];
+    console.log(fitem(format, key, value));
+  }
+  console.log(format.afterList);
+}
+
+function flat(xs) {
+  return [].concat.apply([], xs);
+}
+
+/**
+ * Format Heading
+ * @param {Object} f format
+ * @param {String} h heading
+ */
+function fheading(f, h) {
+  return f.beforeHeading + h + f.afterHeading;
+}
+
+/**
+ * Format item
+ * @param {Object} f format
+ * @param {String} key
+ * @param {String} value
+ */
+function fitem(f, k, v) {
+  let key = f.beforeKey + k + f.afterKey,
+    value = f.beforeValue + v + f.afterValue;
+  return f.beforeItem + key + ' ' + value + f.afterItem; 
 }
